24fa74bae9506dd9114ecd146434957e7d0cb772
perfectjy <perfectjy1@naver.com>
2024-01-10 16:59:04 +0900

    자바스크립트 스터디 및 웹폼 테스트

---
+++ async/async.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// async & await
+// clear style of using promise :)
--- js/array-api.js
+++ js/array-api.js
@@ -62,4 +62,45 @@ const students = [
 // 07. make an array containing only the students' scores
 // result should be: [45, 80, 90, 66, 88]
 {
+  const result = students.map((student) => student.score);
+  console.log(result);
+}
+
+// 08. check if there is a student with the score lower than 50
+{
+  console.clear();
+  const result1 = students.some((student) => student.score < 50); // 어떤 것이라도 하나 만족되는 것이 있는지 없는지 확인할 때
+  const result2 = students.every((student) => student.score >= 50); // 모든 배열의 조건이 맞는지 확인 할 때
+  console.log(result1, result2);
+}
+
+console.clear();
+// 09. compute students' average score
+{
+  // reduce() :: 우리가 원하는 시작 점부터 모든 배열을 돌면서 어떤 값을 누적할 때 쓰는 것 , 하나의 결과값 반환
+  const result = students.reduce((prev, curr) => prev + curr.score, 0);
+  console.log(result / students.length);
+
+  // reduceRight() :: 배열의 제일 뒤에서 부터 시작한다 (순서가 꺼꾸로 호출된다)
+}
+
+// 10. make a string containing all the scores
+// result
+
+{
+  const result = students
+    .map((student) => student.score)
+    .filter((score) => score >= 50)
+    .join();
+  console.log(result);
+}
+
+// bonus! do Q10 sorted in ascending order
+// result should be: '45,66,80,88,90'
+{
+  const result = students
+    .map((student) => student.score)
+    .sort((a, b) => a - b)
+    .join();
+  console.log(result);
 }
